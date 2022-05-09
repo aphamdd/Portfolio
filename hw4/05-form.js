219b8fc0821e7b0f8364aa1342cb9fe537167263
Anthony Pham <anthonyphamdd@gmail.com>
2022-05-09 14:14:59 -0700

    Implemented exercise 5

--- hw4/05-form.js
+++ hw4/05-form.js
@@ -2,31 +2,39 @@ let form = document.querySelector('form');
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-  event.preventDefault();
-  console.log("========== Form Submission ==========");
   let username = form.elements[0];
   let email = form.elements[1];
-  let date = form.elements[4];
+  let date = form.elements[5];
 
-  if (username != null || username != "")
-    console.log("Username:", username.value);
-  else
-    console.log("Username: no submission");
+  let validate = false;
+  if (form.elements[0].value || form.elements[1].value || form.elements[5].value)
+    validate = true;
 
-  if (email != null || email != "")
-    console.log("Email:", email.value);
-  else
-    console.log("Email: no submission");
+  if (validate) {
+    console.log("========== Form Submission ==========");
+    if (username.value)
+      console.log("Username:", username.value);
+    else
+      console.log("Username: no submission");
 
-  if (document.getElementById('yes-radio').checked)
-    console.log("Newsletter: Yes");
-  else if (document.getElementById('no-radio').checked)
-    console.log("Newsletter: No");
-  else
-    console.log("Newsletter: no submission");
+    if (email.value)
+      console.log("Email:", email.value);
+    else
+      console.log("Email: no submission");
 
-  if (date != null || date != "")
-    console.log("Date:", date.value);
+    if (document.getElementById('yes-radio').checked)
+      console.log("Newsletter: Yes");
+    else if (document.getElementById('no-radio').checked)
+      console.log("Newsletter: No");
+    else
+      console.log("Newsletter: no submission");
+
+    if (date.value)
+      console.log("Date:", date.value);
+    else
+      console.log("Date: no submission");
+  }
   else
-    console.log("Date: no submission");
+    console.warn("You must enter some data to submit this form");
+    event.preventDefault();
 }
